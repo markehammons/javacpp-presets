7cd0b1618101de99ee5d118e97c3e3fe4d060256
Mark Hammons <markehammons@gmail.com>
2020-02-24 07:47:37 +0100

    Added the beginning cppbuild.sh file for a wlroots facet

---
+++ wlroots/cppbuild.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# This file is meant to be included by the parent cppbuild.sh script
+if [[ -z "$PLATFORM" ]]; then
+    pushd ..
+    bash cppbuild.sh "$@" wlroots
+    popd
+    exit
+fi
+
+
+WLROOTS_VERSION=0.10.0
+WAYLAND_VERSION=1.18.0
+LIBXML_VERSION=2.9.10
+LIBPNG_VERSION=1.6.35
+
+download https://gitlab.gnome.org/GNOME/libxml2/-/archive/v$LIBXML_VERSION/libxml2-v$LIBXML_VERSION.tar.gz libxml2-v$LIBXML_VERSION.tar.gz
+download https://github.com/glennrp/libpng/archive/v$LIBPNG_VERSION.zip libpng-v$LIBPNG_VERSION.zip
+download https://github.com/swaywm/wlroots/archive/$WLROOTS_VERSION.zip wlroots-$WLROOTS_VERSION.zip
+download https://github.com/wayland-project/wayland/archive/$WAYLAND_VERSION.zip wayland-$WAYLAND_VERSION.zip
+
+
+
+case $PLATFORM in
+    linux-x86_64)
+        mkdir $PLATFORM
+        cd $PLATFORM
+        INSTALL_PATH=`pwd`
+        echo "decompressing archives..."
+        tar xvf ../libxml2-v$LIBXML_VERSION.tar.gz
+        unzip ../libpng-v$LIBPNG_VERSION.zip
+        unzip ../wayland-$WAYLAND_VERSION.zip
+        unzip ../wlroots-$WLROOTS_VERSION.zip
+
+
+        cd libxml2-v$LIBXML_VERSION
+        ./autogen.sh prefix=$INSTALL_PATH --without-python
+        make -j $MAKEJ
+        make install
+
+        cd $INSTALL_PATH/wayland-$WAYLAND_VERSION
+        PKG_CONFIG_PATH=$INSTALL_PATH/lib64/pkgconfig ./autogen.sh prefix=$INSTALL_PATH --disable-documentation
+        make -j $MAKEJ
+        make install
+
+
+        ### WLROOTS DEPENDENCIES
+
+        cd $INSTALL_PATH/libpng-$LIBPNG_VERSION
+        ./configure --prefix=$INSTALL_PATH
+        make -j $MAKEJ
+        make install
+
+        cd $INSTALL_PATH/wlroots-$WLROOTS_VERSION
+        PKG_CONFIG_PATH=$INSTALL_PATH/lib64/pkgconfig meson build
+        PKG_CONFIG_PATH=$INSTALL_PATH/lib64/pkgconfig meson configure --prefix $INSTALL_PATH build
+        ninja -j $MAKEJ -C build
+        ninja -j $MAKEJ -C build install
+        ;;
+    *)
+        echo "Error: Platform \"$PLATFORM\" is not supported"
+        ;;
+esac
+
+cd ../..
